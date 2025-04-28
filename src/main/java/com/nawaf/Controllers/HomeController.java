9fb6d7175a7c1ae92347b3f1769d1cb69e6dbeb8
NawafAlowaifi <NawafAlowaifi@gmail.com>
2025-04-28 21:45:51 +0300

    28/4

---
+++ src/main/java/com/nawaf/Controllers/HomeController.java
@@ -0,0 +1,28 @@
+package com.nawaf.Controllers;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngines;
+import org.camunda.bpm.engine.runtime.ProcessInstantiationBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping(value = "/home",method = RequestMethod.GET)
+    public String home(){
+    return "Hello World";
+    }
+    @RequestMapping(value = "/execute",method = RequestMethod.GET)
+    public String execute(){
+        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+       ProcessInstantiationBuilder instance= processEngine.getRuntimeService().createProcessInstanceByKey("first_bpmn_execute");
+       instance.setVariable("item", "Computer");
+       instance.businessKey("execure-endpoint");
+       instance.executeWithVariablesInReturn();
+        return "executed";
+    }
+
+
+}
